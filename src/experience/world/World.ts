f9b848f34d85c19d8225bec7890d6e84be219f18
LeomharT <leo.liao.lzy@gmail.com>
2026-03-28 11:18:36 +0800

    chore: update rapier world

--- src/experience/world/World.ts
+++ src/experience/world/World.ts
@@ -1,3 +1,4 @@
+import { World as RapierWorld } from '@dimforge/rapier3d';
 import type { Experience } from '../Experience';
 import { Environment } from './Environment';
 import { Floor } from './Floor';
@@ -7,6 +8,7 @@ export class World {
   constructor(exp: Experience) {
     this._exp = exp;
 
+    this._setupRapierWorld();
     this.floor = new Floor(this._exp);
 
     this._exp.resources.addEventListener('ready', () => {
@@ -17,6 +19,8 @@ export class World {
 
   private _exp: Experience;
 
+  private world: RapierWorld;
+
   public floor: Floor;
 
   public env!: Environment;
@@ -24,4 +28,8 @@ export class World {
   public rosCar!: ROSCar;
 
   public update() {}
+
+  private _setupRapierWorld() {
+    console.log(RapierWorld);
+  }
 }
