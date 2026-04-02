9ae4344162ab8804b1690886b77b6e53ae8ae311
LeomharT <leo.liao.lzy@gmail.com>
2026-04-02 11:59:56 +0800

    feat: dittach wheel from car

--- src/experience/world/Car/index.ts
+++ src/experience/world/Car/index.ts
@@ -1,5 +1,5 @@
-import { ColliderDesc, RigidBodyDesc } from '@dimforge/rapier3d';
-import { Mesh, MeshStandardMaterial, Quaternion } from 'three';
+import { ColliderDesc, JointData, RevoluteImpulseJoint, RigidBodyDesc } from '@dimforge/rapier3d';
+import { Mesh, MeshStandardMaterial, Quaternion, Vector3 } from 'three';
 import type { FolderApi } from 'tweakpane';
 import type { Experience } from '../../Experience';
 
@@ -23,8 +23,6 @@ export class Car {
   }
 
   private _initModel() {
-    const q = new Quaternion();
-
     const model = this._exp.resources.items.carModel;
     const mesh = model.scene;
 
@@ -35,38 +33,71 @@ export class Car {
     });
     this._exp.scene.add(mesh);
 
+    // Wheels
     const wheelFL = mesh.getObjectByName('前轮1') as Mesh;
     const wheelFR = mesh.getObjectByName('前轮2') as Mesh;
     const wheelBL = mesh.getObjectByName('后轮1') as Mesh;
     const wheelBR = mesh.getObjectByName('后轮2') as Mesh;
-    // Wheels
+    mesh.updateWorldMatrix(true, true);
+
+    this._exp.scene.attach(wheelFL);
+    this._exp.scene.attach(wheelFR);
+    this._exp.scene.attach(wheelBL);
+    this._exp.scene.attach(wheelBR);
+
+    console.log(mesh);
+
     const wheels = [wheelFL, wheelFR, wheelBL, wheelBR] as const;
 
+    // Car body
     const carBodyDesc = RigidBodyDesc.dynamic();
     carBodyDesc.setTranslation(0, 3, 0);
     carBodyDesc.setCanSleep(false);
+    carBodyDesc.setEnabled(true);
     const carBody = this._exp.physicWorld.instance.createRigidBody(carBodyDesc);
 
-    const carColliderDesc = ColliderDesc.cuboid(1, 2.25, 1);
-    carColliderDesc.setTranslation(0, 2.25, 0);
-    carColliderDesc.setRestitution(0.55);
-    const carCollider = this._exp.physicWorld.instance.createCollider(carColliderDesc, carBody);
+    const carColliderDesc = ColliderDesc.cuboid(0.55, 1.25, 0.55);
+    carColliderDesc.setTranslation(0, 1.25, 0);
+    carColliderDesc.setRestitution(0.25);
+    carColliderDesc.setEnabled(true);
+    this._exp.physicWorld.instance.createCollider(carColliderDesc, carBody);
 
-    return { mesh, wheels, carBody, carCollider, q };
+    const wheelFLBodyDesc = RigidBodyDesc.dynamic();
+    wheelFLBodyDesc.setCanSleep(false);
+    const wheelFLBody = this._exp.physicWorld.instance.createRigidBody(wheelFLBodyDesc);
+
+    const wheelFLColliderDesc = ColliderDesc.cylinder(0.23, 0.45);
+    wheelFLColliderDesc.setRotation(new Quaternion().setFromAxisAngle(new Vector3(1, 0, 0), Math.PI / 2));
+    wheelFLColliderDesc.setFriction(2);
+    this._exp.physicWorld.instance.createCollider(wheelFLColliderDesc, wheelFLBody);
+
+    // Joint wheels to car body
+    const m = this._exp.physicWorld.instance.createImpulseJoint(
+      JointData.revolute(wheelFL.position, new Vector3(0, 0, 0), new Vector3(0, 0, -1)),
+      carBody,
+      wheelFLBody,
+      true,
+    ) as RevoluteImpulseJoint;
+
+    m.configureMotorVelocity(20, 2);
+
+    const wheelBodys = [wheelFLBody];
+
+    return { mesh, wheels, carBody, wheelBodys };
   }
 
   public update() {
-    const t = this.car.carBody.translation();
-    const q = this.car.carBody.rotation();
+    // Update Carbody
+    this.car.mesh.position.copy(this.car.carBody.translation());
+    this.car.mesh.quaternion.copy(this.car.carBody.rotation());
 
-    //Transform
-    this.car.mesh.position.copy(t);
-    // Rotate
-    this.car.mesh.quaternion.set(q.x, q.y, q.z, q.w);
+    for (let i = 0; i < this.car.wheelBodys.length; i++) {
+      this.car.wheels[i].position.copy(this.car.wheelBodys[i].translation());
+      this.car.wheels[i].quaternion.copy(this.car.wheelBodys[i].rotation());
+    }
 
     // Update wheels
     for (const w of this.car.wheels) {
-      w.rotation.z -= 0.111;
     }
   }
 }
