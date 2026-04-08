7c9f4f339dd8cdf669d39112b35ce41290cb349f
LeomharT <leo.liao.lzy@gmail.com>
2026-04-08 10:03:18 +0800

    feat: lerp control polar angle

--- src/experience/Camera.ts
+++ src/experience/Camera.ts
@@ -1,18 +1,21 @@
-import { PerspectiveCamera } from 'three';
+import { MathUtils, PerspectiveCamera } from 'three';
 import { OrbitControls, TrackballControls } from 'three/examples/jsm/Addons.js';
 import type { Experience } from './Experience';
 
+const MAX_POLAR_ANGLE = Math.PI / 2.5;
+const MIN_POLAR_ANGLE = 0;
+const LERP_SPEED = 15;
+
 export default class Camera {
-  constructor(experience: Experience) {
-    this._experience = experience;
+  constructor(exp: Experience) {
+    this._exp = exp;
 
     // Setup
-
     this.instance = this._setupCamera();
     this._controls = this._setupControls();
   }
 
-  private _experience: Experience;
+  private _exp: Experience;
 
   public instance: PerspectiveCamera;
 
@@ -27,13 +30,13 @@ export default class Camera {
   }
 
   private _setupControls() {
-    const controls1 = new OrbitControls(this.instance, this._experience.canvas);
+    const controls1 = new OrbitControls(this.instance, this._exp.canvas);
     controls1.enableDamping = true;
     controls1.enableRotate = true;
     controls1.enablePan = true;
     controls1.enableZoom = false;
 
-    const controls2 = new TrackballControls(this.instance, this._experience.canvas);
+    const controls2 = new TrackballControls(this.instance, this._exp.canvas);
     controls2.noPan = true;
     controls2.noRotate = true;
     controls2.noZoom = false;
@@ -45,22 +48,32 @@ export default class Camera {
   private _setupCamera() {
     const camera = new PerspectiveCamera(
       50,
-      this._experience.sizes.width / this._experience.sizes.height,
+      this._exp.sizes.width / this._exp.sizes.height,
       0.1,
       1000,
     );
     camera.position.set(12, 12, 12);
-    this._experience.scene.add(camera);
+    this._exp.scene.add(camera);
 
     return camera;
   }
 
   public resize() {
-    this.instance.aspect = this._experience.sizes.width / this._experience.sizes.height;
+    this.instance.aspect = this._exp.sizes.width / this._exp.sizes.height;
     this.instance.updateProjectionMatrix();
   }
 
   public update(time: number) {
+    const t = 1.0 - Math.exp(LERP_SPEED * -time);
+
+    const polar = this.control1.getPolarAngle();
+
+    const maxPolarAngle = MathUtils.lerp(polar, MAX_POLAR_ANGLE, t);
+    const minPolarAngle = MathUtils.lerp(polar, MIN_POLAR_ANGLE, t);
+
+    this.control1.maxPolarAngle = maxPolarAngle;
+    this.control1.minPolarAngle = minPolarAngle;
+
     this.control2.update();
     this.control1.update(time);
   }
