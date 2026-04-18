10cb22640b1b5dad37f0fc45870a2fe8402507da
LeomharT <leo.liao.lzy@gmail.com>
2026-04-18 11:11:48 +0800

    feat(ui): add shadcn tailwind setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
 import { UserConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 
 export default {
-  plugins: [wasm()],
+  plugins: [wasm(), tailwindcss()],
+  resolve: { tsconfigPaths: true },
 } as UserConfig;
